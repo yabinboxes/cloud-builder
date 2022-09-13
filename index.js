aa7ccc75a14ca44db458e0615a09088e8aa141e7
yabinmonroy <yabin.cleantechcompany@gmail.com>
2022-09-13 02:28:25 -0700

    new variables

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const wait = require('./wait');
 
 
@@ -6,13 +7,16 @@ const wait = require('./wait');
 async function run() {
   try {
     const ms = core.getInput('milliseconds');
+    const type = core.getInput('type');
+    const user = core.getInput('user');
+    
     core.info(`Waiting ${ms} milliseconds ...`);
+    core.info(`Waiting ${type} type ...`);
+    core.info(`Waiting ${user} user ...`);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    console.log(JSON.stringify(github, null, "\t"));
 
-    core.setOutput('time', new Date().toTimeString());
+   
   } catch (error) {
     core.setFailed(error.message);
   }
