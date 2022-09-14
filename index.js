acdc96ad631b0710d12e98413faaf256bef9be03
yabinboxes <yabin.monroy.1@gmail.com>
2022-09-13 18:06:13 -0700

    http

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const httpClient = require('@actions/http-client');
 
 
 // most @actions toolkit packages have async methods
@@ -8,12 +9,43 @@ async function run() {
     const ms = core.getInput('milliseconds');
     const type = core.getInput('type');
     const user = core.getInput('user');
+    const urlSimpleDeployment = 'https://github.com/yabinboxes/gcp-deploy-cloud-run';
     
     core.info(`Waiting ${ms} milliseconds ...`);
     core.info(`Waiting ${type} type ...`);
     core.info(`Waiting ${user} user ...`);
 
-    console.log(JSON.stringify(github, null, "\t"));
+    if(type !== 'deploy') {
+
+    } else if(type === 'deploy') {
+
+      // execute pulumi simple deployment dispatch
+
+      httpClient.
+
+
+
+      /*axios.post("https://api.github.com/repos/yabinboxes/gcp-deploy-cloud-run/dispatches", 
+        {
+            "event_type": "build",
+            "client_payload": {
+                "unit": false,
+                "integration": true,
+                "repo": "yabinboxes/pulumi-backend"
+            }
+        }, {
+            auth: {
+              username: "",
+              password: ""
+            }
+        });*/
+      
+
+    } else {
+
+    }
+
+    //console.log(JSON.stringify(github, null, "\t"));
 
    
   } catch (error) {
