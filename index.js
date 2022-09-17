74b85e72c4d64eaa747318237e93392273dbb96b
yabinmonroy <yabin.cleantechcompany@gmail.com>
2022-09-16 21:11:09 -0700

    test

--- index.js
+++ index.js
@@ -11,56 +11,78 @@ async function run() {
     const user = core.getInput('user');
     const username = core.getInput('username');
     const password = core.getInput('password');
+    const orgName = core.getInput('org_name');
+    const projectName = core.getInput('project_name');
+    const repo = core.getInput('repo');
+
+
 
     //const octokit = github.getOctokit(password);
 
     //const { context = {} } = github;
     //const { pull_request } = context.payload;
 
-    //const urlSimpleDeployment = 'https://github.com/yabinboxes/gcp-deploy-cloud-run';
-    
+    let typeValue = '';
+    let uiCloudBuilderUrl = '';
+
     core.info(`Waiting ${ms} milliseconds ...`);
     core.info(`Waiting ${type} type ...`);
     core.info(`Waiting ${user} user ...`);
     core.info(`Waiting ${username} username ...`);
+    core.info(`Waiting ${orgName} org name ...`);
+    core.info(`Waiting ${projectName} project name ...`);
+    core.info(`Waiting ${repo} repo ...`);
 
-    if(type !== 'deploy') {
-      core.info(`Deploying ...`);
-    } else if(type === 'deploy') {
+    if (type !== 'deploy') {
 
-      // execute pulumi simple deployment dispatch
+      // read ui cloud app builder to return type value url
+      axios.get(uiCloudBuilderUrl)
+      .then(function (response) {
+        typeValue = response;
+        core.info(response);
+      })
+      .catch(function (error) {
+        core.info(error);
+      });
+     
 
-    
+    } else if (type === 'deploy') {
 
-    axios.post("https://api.github.com/repos/yabinboxes/gcp-deploy-cloud-run/dispatches", 
+      typeValue = 'https://api.github.com/repos/yabinboxes/gcp-deploy-cloud-run/dispatches';
+
+    } 
+
+    // execute pulumi simple deployment dispatch
+    if(typeValue !== '')
+    {
+      axios.post(typeValue,
         {
-            "event_type": "build",
-            "client_payload": {
-                "unit": false,
-                "integration": true,
-                "repo": "yabinboxes/pulumi-backend"
-            }
+          "event_type": "build",
+          "client_payload": {
+            "unit": false,
+            "integration": true,
+            "repo": "yabinboxes/pulumi-backend"
+          }
         }, {
-            auth: {
-              username: username,
-              password: password
-            }
-        })
+        auth: {
+          username: username,
+          password: password
+        }
+      })
         .then(function (response) {
           core.info(response);
         })
         .catch(function (error) {
           core.info(error);
         });
-      
-
     } else {
-      core.info(`none ...`);
+      throw 'empty type value';
     }
+    
 
     //console.log(JSON.stringify(github, null, "\t"));
 
-   
+
   } catch (error) {
     core.setFailed(error.message);
   }
