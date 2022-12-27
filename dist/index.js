97118748348a2625346cf507464505dd92850d35
yabin <yabin@yabin-MS-7D13>
2022-12-26 18:34:30 -0800

    parameters

--- dist/index.js
+++ dist/index.js
@@ -7830,10 +7830,9 @@ async function run() {
 
     const type = core.getInput('type');
     const username = core.getInput('username');
+    const email = core.getInput('email');
     const environment = core.getInput('environment');
     const repoLocation = core.getInput('repo-location');
-    const orgName = core.getInput('org-name');
-    const projectName = core.getInput('project-name');
     const githubToken= core.getInput('github-token');
     const command = core.getInput('command');
     const appName = core.getInput('app-name');
@@ -7842,31 +7841,27 @@ async function run() {
 
 
     core.info(`Waiting ${type} type ...`);
-    core.info(`Waiting ${username} username ...`);
-    core.info(`Waiting ${orgName} org name ...`);
-    core.info(`Waiting ${projectName} project name ...`);
-    core.info(`Waiting ${appName} project name ...`);
     core.info(`Waiting metadata -> ${metadata}`);
 
     
     //https://my-app-2-admin-rlxbkxmq4a-uc.a.run.app
     // request code pulumi code in base of type value
-    axios.get(`${urlEndpoint}/pulumi-endpoints/retrieve/${type}/`)
+      axios.get(`${urlEndpoint}/gitlab-mapping/retrieve/${type}/${email}`)
         .then(function (response) {
 
-          core.info("response -> ", response.data);
+
           axios.post("https://api.github.com/repos/" + response.data + "/dispatches",
             {
               "event_type": "build",
               "client_payload": {
                 "username": username,
+                "email": email,  
                 "environment": environment,
                 "repoLocation": repoLocation,
-                "orgName": orgName,
-                "projectName": projectName,
                 "githubToken": githubToken,
                 "command": command,
-                "appName": appName
+                "appName": appName,
+                "metadata": metadata
               }
             }, {
             auth: {
